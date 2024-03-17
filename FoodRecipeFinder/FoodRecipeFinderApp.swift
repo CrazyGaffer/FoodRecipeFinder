be9cebdadf89e8178b326409913c49f6abce9814
CrazyGaffer <62564443+CrazyGaffer@users.noreply.github.com>
2024-03-17 20:56:28 +0200

    Initial Commit

---
+++ FoodRecipeFinder/FoodRecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipeFinderApp.swift
+//  FoodRecipeFinder
+//
+//  Created by Roman Krusman on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
